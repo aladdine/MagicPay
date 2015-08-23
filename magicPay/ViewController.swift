a7092539866e8b210e6bf9bd14aafd71387cb13a
Lucas Farah <lucas.farah@me.com>
2015-08-22 21:05:49 -0700

    first commit

--- magicPay/ViewController.swift
+++ magicPay/ViewController.swift
@@ -7,19 +7,104 @@
 //
 
 import UIKit
+import DigitsKit
+import CoreLocation
+class ViewController: UIViewController,CLLocationManagerDelegate {
+  
+  var myBeaconRegion:CLBeaconRegion = CLBeaconRegion()
+  var locationManager:CLLocationManager = CLLocationManager()
+  
+  
+  func start() {
+    locationManager = CLLocationManager()
+    locationManager.delegate = self
+    locationManager.desiredAccuracy = kCLLocationAccuracyBestForNavigation
+    locationManager.requestAlwaysAuthorization()
+    locationManager.startUpdatingLocation()
+    
+    
+    let uuid = NSUUID(UUIDString: "3E6857AF-11C7-44AD-837D-F3766E80520B")
+    let uuid2 = NSUUID(UUIDString: "52414449-5553-4E45-5457-4F234B53434F")
 
-class ViewController: UIViewController {
+    self.myBeaconRegion = CLBeaconRegion(proximityUUID: uuid, identifier: "com.GreatAmerica")
 
+//    self.locationManager.startMonitoringForRegion(self.myBeaconRegion)
+    
+    self.locationManager.startRangingBeaconsInRegion(self.myBeaconRegion)
+
+  }
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
+    let authenticateButton = DGTAuthenticateButton(authenticationCompletion: {
+      (session: DGTSession!, error: NSError!) in
+      // play with Digits session
+      println("phone number: \(session.phoneNumber)")
+      println("User id: \(session.userID)")
+      
+    })
+    authenticateButton.center = self.view.center
+    self.view.addSubview(authenticateButton)
+    
+    self.start()
+    // Create a NSUUID with the same UUID as the broadcasting beacon
+    //    NSUUID *uuid = [[NSUUID alloc] initWithUUIDString:@"A77A1B68-49A7-4DBF-914C-760D07FBB87B"];
+    
+    // Setup a new region with that UUID and same identifier as the broadcasting beacon
+    
+    // Tell location manager to start monitoring for the beacon region
   }
-
+  //MARK: LocationManager Delegate
+  
+  func locationManager(manager: CLLocationManager!, monitoringDidFailForRegion region: CLRegion!, withError error: NSError!) {
+    println("Failed")
+  }
+  func locationManager(manager: CLLocationManager, didEnterRegion region: CLRegion) {
+    println("Beacon Found")
+    
+    println(region.identifier)
+  }
+  func locationManager(manager: CLLocationManager!, didRangeBeacons beacons: [AnyObject]!, inRegion region: CLBeaconRegion!) {
+    //    println("Beacon Found")
+    //    println(beacons.first)
+    
+    if (beacons.first != nil)
+    {
+      let nearestBeacon = beacons.first as! CLBeacon
+      var message = ""
+      
+      switch nearestBeacon.proximity {
+      case CLProximity.Far:
+        message = "Proximity: Far"
+      case CLProximity.Near:
+        message = "Proximity: Near"
+      case CLProximity.Immediate:
+        message = "Proximity: Immediate"
+        self.beaconDidApprox(beacon: nearestBeacon, forRegion: region)
+      case CLProximity.Unknown:
+        return
+      }
+//      println(message)
+    }
+  }
+  
+  func beaconDidApprox(#beacon:CLBeacon, forRegion region: CLRegion)
+  {
+    println(region.identifier)
+    let session = DGTSession.userID
+  }
+  func locationManager(manager: CLLocationManager, didExitRegion region: CLRegion) {
+    println("Exited Beacon")
+  }
+  
+  
+  
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.
   }
-
-
+  
+  
 }
 
